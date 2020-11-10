517260fac9bef80c2f7fb44fa18d083cb5a19aa7
Venganzaalchocolate <73942230+Venganzaalchocolate@users.noreply.github.com>
2020-11-11 00:34:17 +0100

    creando barajarbaraja

--- dia_2.py
+++ dia_2.py
@@ -1,13 +1,30 @@
 #Fabricando una baraja
+import random
 
 #o=oros, c=copas, e=espadas y b=bastos
+
+def creacionBaraja(palos,numeroPalos):
+    baraja=[]
+    for tipos in palos:
+        for x in numerosPalos:
+            baraja.append((tipos,x))
+    return baraja
+
 palos=['o','c','e','b']
 numerosPalos=['A','2','3','4','5','6','7','S','C','R']
-baraja={}
 
-for tipo in palos:
-    for numero in numerosPalos:
-        baraja[tipo]=numero
+baraja=(creacionBaraja(palos,numerosPalos))
 
-print(baraja)
-    
+def barajarBaraja(baraja):
+    barajaMezclada=[]
+    barajaMezclada.append(random.choice(baraja))
+    while len(baraja) != len(barajaMezclada):
+        for cartas in baraja:
+            if cartas in not barajaMezclada:
+                barajaMezclada.append(random.choice(baraja))
+    return barajaMezclada
+        
+
+print(len(baraja))
+print(barajarBaraja(baraja))
+print(len(barajarBaraja(baraja)))
